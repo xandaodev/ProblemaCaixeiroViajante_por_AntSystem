495c92b678348c0fae69eb3bc562c6f99ce7d047
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-16 15:07:46 -0300

    adiceionei varias funcoes pra rodar o algoritmo em AntSystem e em pcv, agr to penando com o algoritmo da roleta

--- AntSystem.java
+++ AntSystem.java
@@ -1,28 +1,92 @@
 package ProblemaCaixeiroViajante_por_AntSystem;
 
+import java.util.Random;
+
 public class AntSystem {
     private Pcv pcv;
     private Formiga[] formigas;
     private double[][] feromonios;
     //vetor da melhor trilha universal
 
+    //vetor que gaurda a melhor solucao encontrada ate agr:
+    private int[] melhorTrilhaGlobal;
+    private double melhorDistanciaGlobal = Double.MAX_VALUE;//começa com infinito
+
+
     //parametros que ele deu no slide:
-    /* 
-    private double alpha = 1.0; 
-    private double beta = 5.0;  
-    private double rho = 0.5;   
-    //m = |V| ??? nao sei oq é isso amor
-    private double Q = 100.0;   
-    //private double tau0 = 10 elevado a -6;
-    */
-
-    //metodos que tem que ter: 
-
-    //inicializar os feromonios
-    //atualizar os feromonios
-    //metodo antsystem
-    //metodo de ir executando o algoritmo por N(NUMERO DE ITERACOES) vezes
-    //escolher proxima cidade
-    //....
     
+    private double alpha = 1.0; //influencia do feromonio
+    private double beta = 5.0; //influencia da distancia
+    private double rho = 0.5; //taxa de evaporaçao
+    private double Q = 100.0; //contsante do deposito de feromonio
+    private double tau0 = 1e-6;// 10 elevado a -6 - feromonio inicial
+
+    private Random aleatorio = new Random();
+
+    //contrutor:
+    public AntSystem(Pcv pcv, int numFormigas){
+        this.pcv= pcv;
+        this.formigas =new Formiga[numFormigas];
+        this.feromonios =new double[pcv.getNumCidades()][pcv.getNumCidades()];
+        this.melhorTrilhaGlobal=new int[pcv.getNumCidades()];
+
+        // inicia as formiguinha
+        for(int k = 0;k< numFormigas;k++){
+            formigas[k]=new Formiga(pcv.getNumCidades());
+        }
+        
+        inicializarFeromonios();
+    }
+    
+    //metodo que inicializa os feromonios
+    public void inicializarFeromonios(){
+        for(int i=0;i <pcv.getNumCidades();i++){
+            for (int j = 0; j < pcv.getNumCidades(); j++) {
+                feromonios[i][j] = tau0; [cite: 140]
+            }
+        }
+    }
+
+    //metodo que executa o algoritmo:
+    public void executar(int maxIteracoes){
+        for(int t=0; t<maxIteracoes; t++){
+            //consturir a viagem de cada formiga:
+            construirSolucoes();
+            //atualiza os feromonios:
+            atualizarFeromonios();
+            System.out.println("Iteração " + t + " - Melhor Distância: " + melhorDistanciaGlobal);
+        }
+    }
+
+    //metodo que controi a viagem de cada formiguinha, cada uma faz sua propria viagem:
+    private void constuirSolucoes(){
+        for(Formiga a : formigas){
+            //aqui cada formigas vai pra uma cidade aleatoria
+            a.resetar(aleatorio.nextInt(pcv.getNumCidades()));
+        }
+        //movendo as formgas:
+        int numPassos = pcv.getNumCidades()-1;
+        for(int passo=0;passo <numPassos; passo++){
+            for(Formiga a: formigas){
+                selecionarProximaCidade(a);
+            }
+        }
+        //depois do calculo de todas as formigas, calcular as distancias e atualizar com a melhor
+        for(Formiga a: formigas){
+            double dist = a.calcularDistanciaTotal(pcv);
+            if(dist<melhorDistanciaGlobal){
+                melhorDistanciaGlobal = dist;
+                melhorTrilhaGlobal =a.getTrilha().clone();
+            }
+        }
+    }
+
+    //metodo da roleta que o vinicius tinha feito em aula
+    /*public void selecionarProximaCidade(Formiga a){
+        int i = a.getCidadeAtual();
+        int numCidades= pcv.getNumCidades();
+        double[] probabilidades = new double[numCidades];
+        double somaProbalidade = 0.0;
+    }
+    */
 }
--- Formiga.java
+++ Formiga.java
@@ -3,15 +3,33 @@ package ProblemaCaixeiroViajante_por_AntSystem;
 public class Formiga {
     private boolean[] visitados;
     private double distanciaViagem;
-    private int trilha[];
+    private int trilha[];//aqui fica a sequencia de cidades visitadas
     private int numCidades;
     private int cidadeAtual;
+    private int indiceTrilha; //isso aqui é pra saber qual posicao da trilha vamos preencehr
 
     public Formiga(int numCidades){
         this.visitados = new boolean[numCidades];
-        this.distanciaViagem = 0.0;
         this.trilha = new int[numCidades];
+        this.distanciaViagem = 0.0;
         this.numCidades = numCidades;
+        this.indiceTrilha =0;
+    }
+
+    public void resetar(int cidadeInicial){
+        Arrays.fill(visitados, false); //funcao pra limpar o vetor de visitados
+        this.distanciaViagem =0.0;
+        this.indiceTrilha=0;
+        //coloca na cidade inicial:
+        visitarCidade(cidadeInicial);
+
+    }
+
+    public void visitarCidade(int cidade){
+        visitados[cidade] = true;
+        trilha[indiceTrilha] = cidade;
+        cidadeAtual = cidade;
+        indiceTrilha++;
     }
 
     public boolean foiVisitada(int cidade){
@@ -28,6 +46,18 @@ public class Formiga {
 
     public double getDistanciaViagem(){
         return distanciaViagem;
+    }
+
+    //metodo que calcular a distancia total da viagem
+    public double calcularDistanciaTotal(Pcv pcv){
+        double dist=0;
+        for(int i=0;i<numCidades -1 ;i++){
+            dist =+ pcv.getDistancia(trilha[i], trilha[i+1]);
+        }
+        dist += pcv.getDistancia(trilha[numCidades-1], trilha[0]); [cite: 97]
+
+        this.distanciaViagem =dist;
+        return dist;
     }
 
     
