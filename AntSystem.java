9e22cd77333499f02fad11f2461a0b7c4a28d23f
xandao.dev <alexandrevitaldp@gmail.com>
2025-11-16 15:43:09 -0300

    atualizando funcoes

--- AntSystem.java
+++ AntSystem.java
@@ -2,14 +2,16 @@ package ProblemaCaixeiroViajante_por_AntSystem;
 
 import java.util.Random;
 
+//classe da colonia das formiguinhas
 public class AntSystem {
     private Pcv pcv;
-    private Formiga[] formigas;
-    private double[][] feromonios;
-    //vetor da melhor trilha universal
+    private Formiga[] formigas;//vetor que armazena as formigas da colonia
+    private double[][] feromonios;//matriz que guarda a qtd de feromonios em cada aresta
 
+    //vetor da melhor trilha universal
     //vetor que gaurda a melhor solucao encontrada ate agr:
     private int[] melhorTrilhaGlobal;
+    //guarda o custo da melhor viagem
     private double melhorDistanciaGlobal = Double.MAX_VALUE;//começa com infinito
 
 
@@ -21,6 +23,7 @@ public class AntSystem {
     private double Q = 100.0; //contsante do deposito de feromonio
     private double tau0 = 1e-6;// 10 elevado a -6 - feromonio inicial
 
+    //gera numeros aleatorios
     private Random aleatorio = new Random();
 
     //contrutor:
@@ -34,32 +37,34 @@ public class AntSystem {
         for(int k = 0;k< numFormigas;k++){
             formigas[k]=new Formiga(pcv.getNumCidades());
         }
-        
+        //preenche a matriz de feromonio
         inicializarFeromonios();
     }
     
+    //metodos principais abaixo (coisa pra caralho):
+
     //metodo que inicializa os feromonios
     public void inicializarFeromonios(){
         for(int i=0;i <pcv.getNumCidades();i++){
             for (int j = 0; j < pcv.getNumCidades(); j++) {
-                feromonios[i][j] = tau0; [cite: 140]
+                feromonios[i][j] = tau0;
             }
         }
     }
 
-    //metodo que executa o algoritmo:
+    //metodo que executa o algoritmo, loop principal:
     public void executar(int maxIteracoes){
         for(int t=0; t<maxIteracoes; t++){
             //consturir a viagem de cada formiga:
             construirSolucoes();
-            //atualiza os feromonios:
+            //atualiza os feromonios(hora que o feromonio evapora que o vinicius falou):
             atualizarFeromonios();
             System.out.println("Iteração " + t + " - Melhor Distância: " + melhorDistanciaGlobal);
         }
     }
 
     //metodo que controi a viagem de cada formiguinha, cada uma faz sua propria viagem:
-    private void constuirSolucoes(){
+    private void construirSolucoes(){
         for(Formiga a : formigas){
             //aqui cada formigas vai pra uma cidade aleatoria
             a.resetar(aleatorio.nextInt(pcv.getNumCidades()));
@@ -76,7 +81,7 @@ public class AntSystem {
             double dist = a.calcularDistanciaTotal(pcv);
             if(dist<melhorDistanciaGlobal){
                 melhorDistanciaGlobal = dist;
-                melhorTrilhaGlobal =a.getTrilha().clone();
+                melhorTrilhaGlobal =a.getTrilha().clone();//esse.clone é pra criar a copia da trilha, muito util essa paradinha ai 
             }
         }
     }
@@ -85,8 +90,8 @@ public class AntSystem {
     public void selecionarProximaCidade(Formiga a){
         int i = a.getCidadeAtual();
         int numCidades= pcv.getNumCidades();
-        double[] probabilidades = new double[numCidades];
-        double somaProbabilidades = 0.0;
+        double[] probabilidades = new double[numCidades];//armazena o "desejo" pra cada cidade
+        double somaProbabilidades = 0.0;//soma o desejo
 
         //calculo do desejo que o vinicius flaou em aula e to no slide
         for(int j=0; j< numCidades;j++){
@@ -99,17 +104,56 @@ public class AntSystem {
                 probabilidades[j] = 0.0; // nao pode acessar cidade ja visitada
             }
         }
+        //aqui ele gira a roleta e sorteia um nmr entre 0  e somaProbal=bilidades
         double roleta= aleatorio.nextDouble() * somaProbabilidades;
         double acumulado = 0.0;
         for(int j = 0; j < numCidades; j++){
+            //so considera cidade q n foram visitadas
             if(!a.foiVisitada(j)){
-                acumulado += probabilidades[j];
+                acumulado += probabilidades[j];//acumula o desjeo
                 if(acumulado >=roleta){
                     a.visitarCidade(j);
                     return; // escolheu a cidade => sai do metodo
                 }
             }
         }
+    }
+
+    //metodo de atualizar feromonios
+    public void atualizarFeromonios(){
+        //evaporacao
+        for(int i=0; i<pcv.getNumCidades(); i++){
+            for(int j=0; j< pcv.getNumCidades(); j++){
+                feromonios[i][j] *= (1.0-rho);
+            }
+        }
+        //deposito
+        for(Formiga k : formigas){
+            double deltaTau = Q / k.getDistanciaViagem();
+            int[] trilha = k.getTrilha();
+            for(int p =0;p< trilha.length - 1; p++){
+                int i = trilha[p];
+                int j = trilha[p + 1];
+                //depositando feromonios
+                feromonios[i][j] += deltaTau;
+                feromonios[j][i] += deltaTau;
+            }
+            // Não esquecer a aresta de volta (última -> primeira)
+            int ultimo = trilha[trilha.length - 1];
+            //pega a ultima cidade da  trilha
+            int primeiro =trilha[0];
+            //deposita feromonio na aresta
+            feromonios[ultimo][primeiro] += deltaTau;
+            feromonios[primeiro][ultimo] += deltaTau;
+        }
+    }
+
+    public double getMelhorDistancia(){ 
+        return melhorDistanciaGlobal; 
+    
+    }
+    public int[] getMelhorTrilha(){ 
+        return melhorTrilhaGlobal; 
     }
     
 }
--- Formiga.java
+++ Formiga.java
@@ -1,35 +1,38 @@
 package ProblemaCaixeiroViajante_por_AntSystem;
 
+import java.util.Arrays;
+//classe formiga, cada objeto formiga vai constuir uma viagem diferente, candidata para solucao
 public class Formiga {
-    private boolean[] visitados;
-    private double distanciaViagem;
+    private boolean[] visitados;//quais cidade ja foram visitadas
+    private double distanciaViagem;//custo total da viagem completa
     private int trilha[];//aqui fica a sequencia de cidades visitadas
     private int numCidades;
-    private int cidadeAtual;
+    private int cidadeAtual;//onde a formiguinha ta 
     private int indiceTrilha; //isso aqui é pra saber qual posicao da trilha vamos preencehr
 
+    //construtor
     public Formiga(int numCidades){
         this.visitados = new boolean[numCidades];
         this.trilha = new int[numCidades];
-        this.distanciaViagem = 0.0;
+        this.distanciaViagem = 0.0; 
         this.numCidades = numCidades;
         this.indiceTrilha =0;
     }
 
+    //prepara a formiga pra nova iteracao, vamos chamar esse metodo no inicio de todo iteracao do algoritmo
     public void resetar(int cidadeInicial){
         Arrays.fill(visitados, false); //funcao pra limpar o vetor de visitados
         this.distanciaViagem =0.0;
         this.indiceTrilha=0;
         //coloca na cidade inicial:
         visitarCidade(cidadeInicial);
-
     }
 
     public void visitarCidade(int cidade){
-        visitados[cidade] = true;
+        visitados[cidade] = true;//marca a cidade como visitada
         trilha[indiceTrilha] = cidade;
-        cidadeAtual = cidade;
-        indiceTrilha++;
+        cidadeAtual = cidade;//atualiza a cidade atual
+        indiceTrilha++;//prepara pra proixma visita
     }
 
     public boolean foiVisitada(int cidade){
@@ -51,12 +54,14 @@ public class Formiga {
     //metodo que calcular a distancia total da viagem
     public double calcularDistanciaTotal(Pcv pcv){
         double dist=0;
+        //loop quq vai da primiera cidade ate a penultima
         for(int i=0;i<numCidades -1 ;i++){
-            dist =+ pcv.getDistancia(trilha[i], trilha[i+1]);
+            dist+= pcv.getDistancia(trilha[i], trilha[i+1]);
         }
         dist += pcv.getDistancia(trilha[numCidades-1], trilha[0]); [cite: 97]
-
+        //salva o resultado final
         this.distanciaViagem =dist;
+        //retorna o valor calculado
         return dist;
     }
 
